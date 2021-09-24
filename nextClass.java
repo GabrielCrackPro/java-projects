e4d3ce3c9925856b51a73eb451b006a682698151
GabrielCrackPro <gabrielcrackpro2001@gmail.com>
2021-09-24 23:55:48 +0200

    Timetable bugs fixed 🔧

--- nextClass.java
+++ nextClass.java
@@ -7,57 +7,47 @@ public class nextClass {
         LocalDateTime now = LocalDateTime.now();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyy HH:mm:ss");
         String formatedDate = now.format(formatter);
+        String actualDate = formatedDate.substring(0, 10);
 
         String actualDay = now.getDayOfWeek().toString().substring(0, 3).toLowerCase();
 
-        String[] tirmetable = {};
+        String[] times = { "15:10 - 16:05", "16:05 - 17:00", "17:00 - 17:55", "18:15 - 19:10", "19:10 - 20:05",
+                "20:05 - 21:00" };
+        String[] timetable = {};
         switch (actualDay) {
-            case "Lun":
-                String[] timetable = { "Programación", "Programación", "Entornos de desarrollo",
-                        "Entornos de desarrollo", "Bases de datos", "Bases de datos" };
-                System.out.println("Hoy es " + actualDay);
-                System.out.println("El horario de hoy es ");
-                for (int i = 0; i < timetable.length; i++) {
-                    System.out.println(timetable[i]);
-                }
+            case "mon":
+                actualDay = "Lunes";
+                timetable = new String[] { "Programación", "Programación", "Entornos de desarrollo",
+                        " Entornos de desarrollo", "Bases de datos", "Bases de datos" };
                 break;
-            case "Mar":
-                String[] timetable = { "Ingles", "Ingles", "Programación", "Programación", "Marcas", "Sistemas" };
-                System.out.println("Hoy es " + actualDay);
-                System.out.println("El horario de hoy es ");
-                for (int i = 0; i < timetable.length; i++) {
-                    System.out.println(timetable[i]);
-                }
+            case "tue":
+                actualDay = "Martes";
+                timetable = new String[] { "Ingles", "Ingles", "Programación", "Programación", "Marcas", "Sistemas" };
                 break;
-            case "Mie":
-                timetable = new String[] { "Bases de datos", "FOL", "FOL", "Marcas", "Sistemas", "Sistemas" };
-                System.out.println("Hoy es " + actualDay);
-                System.out.println("El horario de hoy es ");
-                for (int i = 0; i < timetable.length; i++) {
-                    System.out.println(timetable[i]);
-                }
+            case "wed":
+                actualDay = "Miércoles";
+                timetable = new String[] { "Bases de datos", "FOL", " FOL", "Marcas", "Sistemas", "Sistemas" };
                 break;
-            case "Jue":
+            case "thu":
+                actualDay = "Jueves";
                 timetable = new String[] { "Bases de datos", "Bases de datos", "Programación", "Programación",
                         "Sistemas", "Sistemas" };
-                System.out.println("Hoy es " + actualDay);
-                System.out.println("El horario de hoy es ");
-                for (int i = 0; i < timetable.length; i++) {
-                    System.out.println(timetable[i]);
-                }
                 break;
-            case "Vie":
+            case "fri":
+                actualDay = "Viernes";
                 timetable = new String[] { "Marcas", "FOL", "Entornos de desarrollo", "Ingles", "Programación",
                         "Programación" };
-                System.out.println("Hoy es " + actualDay);
-                System.out.println("El horario de hoy es ");
-                for (int i = 0; i < timetable.length; i++) {
-                    System.out.println(timetable[i]);
-                }
                 break;
             default:
-                System.out.println("No hay clases");
+                System.out.println("Hoy no hay clases");
                 break;
+        }
+        String dateString = "Hoy es " + actualDay + " " + actualDate + ", el horario de hoy es";
+        System.out.println(dateString);
+        System.out.println("-------------------------------");
+        for (int i = 0; i < timetable.length; i++) {
+            System.out.println(times[i] + " " + timetable[i]);
+            System.out.println("--------------------------------");
         }
     }
 }
