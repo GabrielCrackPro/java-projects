d9f18973a1f9379956dc362c96204cba9e4c161d
GabrielCrackPro <gabrielcrackpro2001@gmail.com>
2021-10-01 23:55:03 +0200

    docs deleted ✏️

--- Base Converter/docs/member-search-index.js
+++
@@ -1,1 +0,0 @@
-memberSearchIndex = [{"p":"<Unnamed>","c":"baseConverter","l":"baseConverter()","url":"%3Cinit%3E()"},{"p":"<Unnamed>","c":"baseConverter","l":"convert(int, int)","url":"convert(int,int)"},{"p":"<Unnamed>","c":"baseConverter","l":"main(String[])","url":"main(java.lang.String[])"}]
