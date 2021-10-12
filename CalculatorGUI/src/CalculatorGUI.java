7ef80e9f41644451b0aec7a887637b010f890aad
GabrielCrackPro <gabrielcrackpro2001@gmail.com>
2021-10-12 14:40:07 +0200

    Calculator GUI - Button actions amd operations added 💻

--- CalculatorGUI/src/CalculatorGUI.java
+++ CalculatorGUI/src/CalculatorGUI.java
@@ -1,8 +1,8 @@
 import javax.swing.*;
 import java.awt.Font;
 import java.awt.FlowLayout;
-import java.awt.Cursor;
 import java.awt.GridLayout;
+import java.awt.Cursor;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -13,7 +13,7 @@ public class CalculatorGUI implements Runnable {
         frame.setLocation(400, 300);
         frame.setLayout(new FlowLayout());
         frame.setSize(400, 400);
-        frame.setPreferredSize(frame.getSize());
+        frame.setResizable(false);
         frame.setVisible(true);
 
         JTextField textField = new JTextField(20);
@@ -22,74 +22,149 @@ public class CalculatorGUI implements Runnable {
         textField.setPreferredSize(textField.getSize());
         textField.setHorizontalAlignment(JTextField.LEFT);
         textField.setEditable(false);
+
         frame.add(textField);
 
-        JButton[] oneToThree = new JButton[4];
-        JPanel oneToThreePanel = new JPanel();
-        for (int i = 1; i <= 3; i++) {
-            oneToThree[i] = new JButton(Integer.toString(i));
-            oneToThree[i].setFont(new Font("Arial", Font.PLAIN, 20));
-            oneToThree[i].setSize(70, 70);
-            oneToThree[i].setCursor(new Cursor(Cursor.HAND_CURSOR));
-            oneToThree[i].setPreferredSize(oneToThree[i].getSize());
-            oneToThree[i].addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    textField.setText(textField.getText() + ((JButton) e.getSource()).getText());
-                }
-            });
-            oneToThreePanel.add(oneToThree[i]);
-            oneToThreePanel.setLayout(new GridLayout(1, 3));
-            frame.add(oneToThreePanel);
-        }
-        JButton[] fourToSix = new JButton[7];
-        JPanel fourToSixPanel = new JPanel();
-        for (int i = 4; i <= 6; i++) {
-            fourToSix[i] = new JButton(Integer.toString(i));
-            fourToSix[i].setFont(new Font("Arial", Font.PLAIN, 20));
-            fourToSix[i].setSize(70, 70);
-            fourToSix[i].setPreferredSize(fourToSix[i].getSize());
-            fourToSix[i].addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    textField.setText(textField.getText() + ((JButton) e.getSource()).getText());
-                }
-            });
-            fourToSixPanel.add(fourToSix[i]);
-            fourToSixPanel.setLayout(new GridLayout(1, 3));
-            frame.add(fourToSixPanel);
-        }
-        JButton[] sevenToNine = new JButton[10];
-        JPanel sevenToNinePanel = new JPanel();
-        for (int i = 7; i <= 9; i++) {
-            sevenToNine[i] = new JButton(Integer.toString(i));
-            sevenToNine[i].setFont(new Font("Arial", Font.PLAIN, 20));
-            sevenToNine[i].setSize(70, 70);
-            sevenToNine[i].setPreferredSize(sevenToNine[i].getSize());
-            sevenToNine[i].addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    textField.setText(textField.getText() + ((JButton) e.getSource()).getText());
-                }
-            });
-            sevenToNinePanel.add(sevenToNine[i]);
-            sevenToNinePanel.setLayout(new GridLayout(1, 3));
-            frame.add(sevenToNinePanel);
-        }
-        JButton zero = new JButton("0");
-        JPanel zeroPanel = new JPanel();
-        zero.setFont(new Font("Arial", Font.PLAIN, 20));
-        zero.setSize(70, 70);
-        zero.setPreferredSize(zero.getSize());
-        zero.addActionListener(new ActionListener() {
+        ActionListener listener = new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                textField.setText(textField.getText() + ((JButton) e.getSource()).getText());
+                if (e.getActionCommand().equals("AC")) {
+                    textField.setText("");
+                } else if (e.getActionCommand().equals("+/-")) {
+                    String text = textField.getText();
+                    if (text.charAt(0) == '-') {
+                        textField.setText(text.substring(1));
+                    } else {
+                        textField.setText("-" + text);
+                    }
+                } else if (e.getActionCommand().equals("%")) {
+                    String text = textField.getText();
+                    textField.setText(text + "%");
+                } else if (e.getActionCommand().equals("=")) {
+                    String text = textField.getText();
+                    textField.setText(calculate(text));
+                } else {
+                    String text = textField.getText();
+                    textField.setText(text + e.getActionCommand());
+                }
+
             }
-        });
-        zeroPanel.add(zero);
-        zeroPanel.setLayout(new GridLayout(1, 3));
-        frame.add(zeroPanel);
+
+            private String calculate(String text) {
+                String[] parts = text.split("[+\\-\\*\\/]");
+                double result = Double.parseDouble(parts[0]);
+                if (parts.length > 2) {
+                    return "Max numbers allowed: 2";
+                }
+                for (int i = 1; i < parts.length; i++) {
+                    if (text.charAt(i) == '+') {
+                        result += Double.parseDouble(parts[i]);
+                    } else if (text.charAt(i) == '-') {
+                        result -= Double.parseDouble(parts[i]);
+                    } else if (text.charAt(i) == '*') {
+                        result *= Double.parseDouble(parts[i]);
+                    } else if (text.charAt(i) == '/') {
+                        result /= Double.parseDouble(parts[i]);
+                    }
+                }
+                return String.valueOf(result);
+            }
+        };
+
+        JButton[] firstRow = new JButton[4];
+        JPanel firstRowPanel = new JPanel();
+
+        firstRowPanel.setLayout(new GridLayout(1, 4));
+        firstRow[0] = new JButton("AC");
+        firstRow[1] = new JButton("+/-");
+        firstRow[2] = new JButton("%");
+        firstRow[3] = new JButton("/");
+
+        for (int i = 0; i < 4; i++) {
+            firstRow[i].setFont(new Font("Arial", Font.PLAIN, 20));
+            firstRow[i].setSize(50, 50);
+            firstRow[i].setPreferredSize(firstRow[i].getSize());
+            firstRow[i].setCursor(new Cursor(Cursor.HAND_CURSOR));
+            firstRow[i].addActionListener(listener);
+            firstRowPanel.add(firstRow[i]);
+        }
+        frame.add(firstRowPanel);
+
+        JButton[] secondRow = new JButton[4];
+        JPanel secondRowPanel = new JPanel();
+        secondRowPanel.setLayout(new GridLayout(1, 4));
+        secondRow[0] = new JButton("7");
+        secondRow[1] = new JButton("8");
+        secondRow[2] = new JButton("9");
+        secondRow[3] = new JButton("*");
+
+        for (int i = 0; i < 4; i++) {
+            secondRow[i].setFont(new Font("Arial", Font.PLAIN, 20));
+            secondRow[i].setSize(50, 50);
+            secondRow[i].setPreferredSize(secondRow[i].getSize());
+            secondRow[i].setCursor(new Cursor(Cursor.HAND_CURSOR));
+            secondRow[i].addActionListener(listener);
+            secondRowPanel.add(secondRow[i]);
+        }
+        frame.add(secondRowPanel);
+
+        JButton[] thirdRow = new JButton[4];
+        JPanel thirdRowPanel = new JPanel();
+        thirdRowPanel.setLayout(new GridLayout(1, 4));
+        thirdRow[0] = new JButton("4");
+        thirdRow[1] = new JButton("5");
+        thirdRow[2] = new JButton("6");
+        thirdRow[3] = new JButton("-");
+
+        for (int i = 0; i < 4; i++) {
+            thirdRow[i].setFont(new Font("Arial", Font.PLAIN, 20));
+            thirdRow[i].setSize(50, 50);
+            thirdRow[i].setPreferredSize(thirdRow[i].getSize());
+            thirdRow[i].setCursor(new Cursor(Cursor.HAND_CURSOR));
+            thirdRow[i].addActionListener(listener);
+            thirdRowPanel.add(thirdRow[i]);
+        }
+        frame.add(thirdRowPanel);
+
+        JButton[] fourthRow = new JButton[4];
+        JPanel fourthRowPanel = new JPanel();
+        fourthRowPanel.setLayout(new GridLayout(1, 4));
+        fourthRow[0] = new JButton("1");
+        fourthRow[1] = new JButton("2");
+        fourthRow[2] = new JButton("3");
+        fourthRow[3] = new JButton("+");
+
+        for (int i = 0; i < 4; i++) {
+            fourthRow[i].setFont(new Font("Arial", Font.PLAIN, 20));
+            fourthRow[i].setSize(50, 50);
+            fourthRow[i].setPreferredSize(fourthRow[i].getSize());
+            fourthRow[i].setCursor(new Cursor(Cursor.HAND_CURSOR));
+            fourthRow[i].addActionListener(listener);
+            fourthRowPanel.add(fourthRow[i]);
+        }
+        frame.add(fourthRowPanel);
+
+        JButton[] fifthRow = new JButton[4];
+        JPanel fifthRowPanel = new JPanel();
+        fifthRowPanel.setLayout(new GridLayout(1, 4));
+        fifthRow[0] = new JButton("0");
+        fifthRow[1] = new JButton(".");
+        fifthRow[2] = new JButton("=");
+        fifthRow[3] = new JButton("");
+
+        fifthRow[3].setVisible(false);
+
+        for (int i = 0; i < 4; i++) {
+            fifthRow[i].setFont(new Font("Arial", Font.PLAIN, 20));
+            fifthRow[i].setSize(50, 50);
+            fifthRow[i].setPreferredSize(fifthRow[i].getSize());
+            fifthRow[i].setCursor(new Cursor(Cursor.HAND_CURSOR));
+            fifthRow[i].addActionListener(listener);
+            fifthRowPanel.add(fifthRow[i]);
+        }
+
+        frame.add(fifthRowPanel);
+
     }
 
     public static void main(String[] args) {
